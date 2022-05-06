36d98ad3826d49a6a7dbbd8d21ea10eedc1e1183
Emerson-Britto <emersonbritto987@gmail.com>
2022-05-06 06:09:46 -0300

    start radio with try/catch

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const RadioCore = require('./src/radio/core');
 const app = express();
 const httpsServer = require('http').Server(app);
 const io = require('socket.io')(httpsServer);
-const radio = new RadioCore(io);
-radio.start();
+
+try {
+	const radio = new RadioCore(io);
+	radio.start();
+} catch(err) {
+	console.error(err);
+}
 
 module.exports = { httpsServer, io, app };
